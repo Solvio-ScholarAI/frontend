e5512e002025d2cda27d7ce1ad7f5a8199662366
Mst.Metaly Khatun <119936956+mstMetaly@users.noreply.github.com>
2025-12-02 15:58:41 +0600

    workflow interface

---
+++ app/interface/workflows/page.tsx
@@ -0,0 +1,9 @@
+import { WelcomeScreen } from "@/components/layout/WelcomeScreen"
+
+export default function WorkflowsPage() {
+    return (
+        <div className="h-full">
+            <WelcomeScreen />
+        </div>
+    )
+} 
